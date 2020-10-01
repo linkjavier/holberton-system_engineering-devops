2235723d8f11f4cbd65131564c87ffc1ca146080
linkjavier <linkjavier@hotmail.com>
2020-09-30 21:40:30 -0500

    Puppy Script that fixed a .php extension config file

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+#fixed Apache because is returning a 500 error
+file { '/var/www/html/wp-includes/class-wp-locale.phpp':
+  ensure => file,
+  source => '/var/www/html/wp-includes/class-wp-locale.php',
+  mode   => '0664'
+}
